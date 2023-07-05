4acb782010cda9dc1423ffdfeade7de6887dbbbc
Joeldamato <damatojoel25@gmail.com>
2023-07-04 23:28:29 -0300

    fixed mode dark

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -31,7 +31,7 @@ export default function Projects(darkMode) {
   return (
     <>
     
-    <div className={` mob:hidden shadow-lg   flex flex-col ${darkMode.dark ? 'bg-gray-700/40 border-[1px] border-violet-950 text-violet-800' : 'bg-white/80'} bg-white/80 rounded-lg w-[22%] min-h-[20%] items-center `}>
+    <div className={` mob:hidden shadow-lg   flex flex-col ${darkMode.dark ? 'bg-black/30   text-violet-800' : 'bg-white/80'} bg-white/80 rounded-lg w-[22%] min-h-[20%] items-center `}>
     <h6 className="mt-5 text-xl text-center font-semibold font-montserrat tracking-widest ">
                      MIS PROYECTOS</h6>
      <ButtonGroup variant="text" className="mt-5 flex flex-col w-[80%] gap-5" >
